4e9b5f31805f48c37df84e583fe68e317a428309
Canh <safetycanh@gmail.com>
2019-03-17 22:59:00 +0700

    prepare for deploy

--- server.js
+++ server.js
@@ -49,7 +49,13 @@ app.use('/api/posts', postsRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+// server static assets if is production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
